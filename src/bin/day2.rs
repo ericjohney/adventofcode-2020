8a30e8df96842e8550a520d0597b504121222cfa
Eric Johney <ejohney@soxhub.com>
2020-12-02 08:13:45 +0000

    adding day 2 solution

---
+++ src/bin/day2.rs
@@ -0,0 +1,64 @@
+extern crate regex;
+
+use regex::Regex;
+use std::collections::HashMap;
+use std::ops::RangeInclusive;
+
+struct Line {
+    start: i64,
+    end: i64,
+    character: char,
+    password: String,
+}
+
+fn main() {
+    let text = include_str!("../../inputs/day2.txt");
+
+    let re = Regex::new(r"(?m)(\d+)-(\d+) (\w): (\w+)").unwrap();
+    let lines = re
+        .captures_iter(text)
+        .map(|c| Line {
+            start: str::parse::<i64>(&c[1]).unwrap(),
+            end: str::parse::<i64>(&c[2]).unwrap(),
+            character: c[3].to_string().chars().nth(0).unwrap(),
+            password: c[4].to_string(),
+        })
+        .collect();
+
+    part1(&lines);
+    part2(&lines);
+}
+
+fn part1(lines: &Vec<Line>) {
+    let valid_count = lines
+        .iter()
+        .filter(|line| {
+            let mut frequency = HashMap::new();
+            line.password.chars().for_each(|c| {
+                *frequency.entry(c).or_insert(0) += 1;
+            });
+            let appearances = RangeInclusive::new(line.start, line.end);
+            appearances.contains(frequency.get(&line.character).unwrap_or(&0i64))
+        })
+        .count();
+    println!("part1 valid count: {}", valid_count);
+}
+
+fn part2(lines: &Vec<Line>) {
+    let valid_count = lines
+        .iter()
+        .filter(|line| {
+            let chars = line.password.chars().collect::<Vec<char>>();
+            let matches = (
+                chars[(line.start - 1) as usize] == line.character,
+                chars[(line.end - 1) as usize] == line.character,
+            );
+            match matches {
+                (false, true) => true,
+                (true, false) => true,
+                _ => false,
+            }
+        })
+        .count();
+    println!("part2 valid count: {}", valid_count);
+}
